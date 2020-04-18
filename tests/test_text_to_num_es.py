f1e69398f4617dae8b4d3455d25acc4db4722ede
pablo <pablorodriper@gmail.com>
2020-04-18 18:47:05 +0200

    Implement Spanish (except ord2card)

---
+++ tests/test_text_to_num_es.py
@@ -0,0 +1,182 @@
+# MIT License
+
+# Copyright (c) 2018-2019 Groupe Allo-Media
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+"""
+Test the ``text_to_num`` library.
+"""
+from unittest import TestCase
+from text_to_num import alpha2digit, text2num
+
+
+class TestTextToNumES(TestCase):
+    def test_text2num(self):
+        self.assertEqual(text2num("cero", "es"), 0)
+        self.assertEqual(text2num("uno", "es"), 1)
+        self.assertEqual(text2num("nueve", "es"), 9)
+        self.assertEqual(text2num("diez", "es"), 10)
+        self.assertEqual(text2num("once", "es"), 11)
+        self.assertEqual(text2num("diecinueve", "es"), 19)
+        self.assertEqual(text2num("veinte", "es"), 20)
+        self.assertEqual(text2num("veintiuno", "es"), 21)
+        self.assertEqual(text2num("treinta", "es"), 30)
+        self.assertEqual(text2num("treinta y uno", "es"), 31)
+        self.assertEqual(text2num("treinta y dos", "es"), 32)
+        self.assertEqual(text2num("treinta y nueve", "es"), 39)
+        self.assertEqual(text2num("noventa y nueve", "es"), 99)
+        self.assertEqual(text2num("cien", "es"), 100)
+        self.assertEqual(text2num("ciento uno", "es"), 101)
+        self.assertEqual(text2num("doscientos", "es"), 200)
+        self.assertEqual(text2num("doscientos uno", "es"), 201)
+        self.assertEqual(text2num("mil", "es"), 1000)
+        self.assertEqual(text2num("mil uno", "es"), 1001)
+        self.assertEqual(text2num("dos mil", "es"), 2000)
+        self.assertEqual(text2num("dos mil noventa y nueve", "es"), 2099)
+        self.assertEqual(text2num("nueve mil novecientos noventa y nueve", "es"), 9999)
+        self.assertEqual(text2num("novecientos noventa y nueve mil novecientos noventa y nueve", "es"), 999999)
+
+        self.assertEqual(alpha2digit("uno coma uno", "es"), '1.1')
+        self.assertEqual(alpha2digit("uno coma cuatrocientos uno", "es"), '1.401')
+        #self.assertEqual(alpha2digit("cero coma cinco", "es"), '0.5')
+
+
+        test1 = "cincuenta y tres mil millones doscientos cuarenta y tres mil setecientos veinticuatro"
+        self.assertEqual(text2num(test1, "es"), 53_000_243_724)
+
+        test2 = (
+            "cincuenta y un millones quinientos setenta y ocho mil trescientos dos"
+        )
+        self.assertEqual(text2num(test2, "es"), 51_578_302)
+
+        test3 = "ochenta y cinco"
+        self.assertEqual(text2num(test3, "es"), 85)
+
+        test4 = "ochenta y uno"
+        self.assertEqual(text2num(test4, "es"), 81)
+
+        self.assertEqual(text2num("quince", "es"), 15)
+        self.assertEqual(text2num("ciento quince", "es"), 115)
+        self.assertEqual(text2num("setenta y cinco mil", "es"), 75000)
+        self.assertEqual(text2num("mil novecientos veinte", "es"), 1920)
+
+    def test_text2num_exc(self):
+        self.assertRaises(ValueError, text2num, "mil mil doscientos", "es")
+        self.assertRaises(ValueError, text2num, "sesenta quince", "es")
+        self.assertRaises(ValueError, text2num, "sesenta cien", "es")
+
+    def test_text2num_zeroes(self):
+        self.assertEqual(text2num("cero", "es"), 0)
+        self.assertEqual(text2num("cero ocho", "es"), 8)
+        self.assertEqual(text2num("cero cero ciento veinticinco", "es"), 125)
+        self.assertRaises(ValueError, text2num, "cinco cero", "es")
+        self.assertRaises(ValueError, text2num, "cincuenta cero tres", "es")
+        self.assertRaises(ValueError, text2num, "cincuenta y tres cero", "es")
+
+    def test_alpha2digit_integers(self):
+        source = "veinticinco vacas, doce gallinas y ciento veinticinco kg de patatas."
+        expected = "25 vacas, 12 gallinas y 125 kg de patatas."
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+        source = "mil doscientos sesenta y seis dolares."
+        expected = "1266 dolares."
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+        source = "un dos tres cuatro veinte quince"
+        expected = "1 2 3 4 20 15"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+        source = "veintiuno, treinta y uno."
+        expected = "21, 31."
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+    def test_relaxed(self):
+        source = "un dos tres cuatro treinta cinco."
+        expected = "1 2 3 4 35."
+        self.assertEqual(alpha2digit(source, "es", relaxed=True), expected)
+
+        source = "un dos tres cuatro veinte, cinco."
+        expected = "1 2 3 4 20, 5."
+        self.assertEqual(alpha2digit(source, "es", relaxed=True), expected)
+
+        source = "treinta y cuatro = treinta cuatro"
+        expected = "34 = 34"
+        self.assertEqual(alpha2digit(source, "es", relaxed=True), expected)
+
+    def test_alpha2digit_formal(self):
+        source = "mas treinta y tres nueve sesenta cero seis doce veintiuno"
+        expected = "+33 9 60 06 12 21"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+        source = "cero nueve sesenta cero seis doce veintiuno"
+        expected = "09 60 06 12 21"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+    def test_and(self):
+        source = "cincuenta sesenta treinta y once"
+        expected = "50 60 30 y 11"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+    def test_alpha2digit_zero(self):
+        source = "trece mil cero noventa"
+        expected = "13000 090"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+        self.assertEqual(alpha2digit("cero", "es"), "0")
+
+    def test_alpha2digit_ordinals(self):
+        source = (
+            "Fifth third second twenty-first hundredth one thousand two hundred thirtieth."
+        )
+        expected = "5th third second 21st 100th 1230th."
+        #self.assertEqual(alpha2digit(source, "es"), expected)
+
+    def test_alpha2digit_decimals(self):
+        source = (
+            "doce coma noventa y nueve, ciento veinte coma cero cinco,"
+            " uno coma doscientos treinta y seis, uno coma dos tres seis."
+        )
+        expected = "12.99, 120.05, 1.236, 1.236."
+        #self.assertEqual(alpha2digit(source, "es"), expected)
+
+        self.assertEqual(alpha2digit("coma quince", "es"), "0.15")
+        self.assertEqual(alpha2digit("cero coma quince", "es"), "0.15")
+
+    def test_alpha2digit_signed(self):
+        source = "Tenemos mas veinte grados dentro y menos quince fuera."
+        expected = "Tenemos +20 grados dentro y -15 fuera."
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+    def test_one_as_noun_or_article(self):
+        source = "Un momento por favor! treinta y un gatos. Uno dos tres cuatro!"
+        expected = "Un momento por favor! 31 gatos. 1 2 3 4!"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+        # End of segment
+        source = "Ni uno. Uno uno. Treinta y uno"
+        expected = "Ni uno. 1 1. 21"
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+    def test_second_as_time_unit_vs_ordinal(self):
+        source = "Un segundo por favor! Decimosegundo se parsea como decimosegundo y es diferente que diez segundos."
+        expected = "Un segundo por favor! 12º se parsea como 12º y es diferente que 10 segundos."
+        self.assertEqual(alpha2digit(source, "es"), expected)
+
+
---
+++ text_to_num/lang/spanish.py
@@ -0,0 +1,147 @@
+# MIT License
+
+# Copyright (c) 2018-2019 Groupe Allo-Media
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import Dict, Optional, Set
+
+from .base import Language
+
+#
+# CONSTANTS
+# Built once on import.
+#
+
+# Those words multiplies lesser numbers (see Rules)
+# Exception: "(de) milliards" that can multiply bigger numbers ("milliards de milliards")
+# Special case: "cent" is processed apart.
+MULTIPLIERS = {
+    "mil": 1000,
+    "miles": 1000,
+    "millones": 1000000,
+    "mil millones": 1000000000,
+}
+
+
+# Units are terminals (see Rules)
+UNITS: Dict[str, int] = {
+    word: value
+    for value, word in enumerate(
+        "uno dos tres cuatro cinco seis siete ocho nueve".split(), 1
+    )
+}
+# Unit variants
+UNITS["un"] = 1
+
+# Single tens are terminals (see Rules)
+STENS: Dict[str, int] = {
+    word: value
+    for value, word in enumerate(
+        "diez once doce trece catorce quince dieciseis diecisiete dieciocho diecinueve veinte veintiuno veintidos veintitres veinticuatro veinticinco veintiseis veintisiete veintiocho veintinueve".split(),
+        10,
+    )
+}
+
+
+# Ten multiples
+# Ten multiples may be followed by a unit only;
+MTENS: Dict[str, int] = {
+    word: value * 10
+    for value, word in enumerate(
+        "treinta cuarenta cincuenta sesenta setenta ochenta noventa".split(), 3
+    )
+}
+
+# Ten multiples that can be combined with STENS
+MTENS_WSTENS: Set[str] = set()
+
+# "cent" has a special status (see Rules)
+HUNDRED = {"cien": 100, "ciento": 100, "doscientos": 200, "trescientos": 300, "cuatrocientos": 400, "quinientos": 500, "seiscientos": 600, "setecientos": 700, "ochocientos": 800, "novecientos": 900}
+
+
+COMPOSITES: Dict[str, int] = {
+    "-".join((ten_word, unit_word)): ten_val + unit_val
+    for ten_word, ten_val in MTENS.items()
+    for unit_word, unit_val in UNITS.items()
+}
+
+# All number words
+
+NUMBERS = MULTIPLIERS.copy()
+NUMBERS.update(UNITS)
+NUMBERS.update(STENS)
+NUMBERS.update(MTENS)
+NUMBERS.update(HUNDRED)
+NUMBERS.update(COMPOSITES)
+
+
+class Spanish(Language):
+
+    MULTIPLIERS = MULTIPLIERS
+    UNITS = UNITS
+    STENS = STENS
+    MTENS = MTENS
+    MTENS_WSTENS = MTENS_WSTENS
+    HUNDRED = HUNDRED
+    NUMBERS = NUMBERS
+
+    SIGN = {"mas": "+", "menos": "-"}
+    ZERO = {"cero"}
+    DECIMAL_SEP = "coma"
+    DECIMAL_SYM = "."
+
+    AND_NUMS = {"un", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"}
+    AND = "y"
+    NEVER_IF_ALONE = {"Un"}
+
+    # Relaxed composed numbers (two-words only)
+    # start => (next, target)
+    RELAXED = {}
+
+    def ord2card(self, word: str) -> Optional[str]:
+        """Convert ordinal number to cardinal.
+
+        Return None if word is not an ordinal or is better left in letters
+        as is the case for fist and second.
+        """
+        return None
+        
+        """
+        plur_suff = word.endswith("ièmes")
+        sing_suff = word.endswith("ième")
+        if not (plur_suff or sing_suff):
+            return None
+        source = word[:-5] if plur_suff else word[:-4]
+        if source == "cinqu":
+            source = "cinq"
+        elif source == "neuv":
+            source = "neuf"
+        elif source not in self.NUMBERS:
+            source = source + "e"
+            if source not in self.NUMBERS:
+                return None
+        return source"""
+
+    def num_ord(self, digits: str, original_word: str) -> str:
+        """Add suffix to number in digits to make an ordinal"""
+        return f"{digits}º" if original_word.endswith("o") else f"{digits}ª"
+
+    def normalize(self, word: str) -> str:
+        return word
