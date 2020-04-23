3c8512faf12d6ace519ce611cbe36e18a9d87bab
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2020-04-23 11:24:41 +0200

    Fixed missing import.

--- text_to_num/lang/spanish.py
+++ text_to_num/lang/spanish.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from typing import Dict, Optional, Set
+from typing import Dict, Optional, Set, Tuple
 
 from .base import Language
 
